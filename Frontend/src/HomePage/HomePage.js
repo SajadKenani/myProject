ce73e033048d9ce23f51fed74e6a0af23de50ccf
SajadKenani <114795074+SajadKenani@users.noreply.github.com>
2023-10-05 22:13:26 +0300

    Update HomePage.js

--- Frontend/src/HomePage/HomePage.js
+++ Frontend/src/HomePage/HomePage.js
@@ -1,4 +1,4 @@
-import { myData, myMessagesDataSection} from "/MyData.js";
+import { myData, myMessagesDataSection} from "../src/HomePage/MyData.js";
 
 addEventListener("DOMContentLoaded", function() {
 
